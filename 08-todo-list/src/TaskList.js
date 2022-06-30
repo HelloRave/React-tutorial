57e8efb20d7e0078c507eb031334f071967173ba
HelloRave <huang.weiwei@trentglobal.edu.sg>
2022-06-30 03:16:48 +0000

    Completed update of CRUD

--- 08-todo-list/src/TaskList.js
+++ 08-todo-list/src/TaskList.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default class TaskList extends React.Component{
+export default class TaskList extends React.Component {
     state = {
         task: [
             {
@@ -19,7 +19,9 @@ export default class TaskList extends React.Component{
                 done: false
             }
         ],
-        newTaskName: ''
+        newTaskName: '',
+        taskBeingEdited: null, //alternatively, store the _id of the task that is being edited
+        modifiedTaskName: ''
     }
 
     updateFormField = (event) => {
@@ -42,11 +44,11 @@ export default class TaskList extends React.Component{
     }
 
     updateTaskDone = (task) => {
-        let clonedTask = {...task, done: !task.done} //last instance of repeated key will overwrite other prev instances
-        
+        let clonedTask = { ...task, done: !task.done } //last instance of repeated key will overwrite other prev instances
+
         // 1. find index of modified task
         let index = this.state.task.findIndex((t) => {
-            if(t._id === clonedTask._id){
+            if (t._id === clonedTask._id) {
                 return true;
             } else {
                 return false
@@ -59,23 +61,79 @@ export default class TaskList extends React.Component{
         })
     }
 
-    render(){
+    beginEditTask = (task) => {
+        this.setState({
+            taskBeingEdited: task
+        })
+    }
+
+    displayTask = (task) => {
+        return (
+            <li className='mt-3'>
+                {task.description}
+                <input type='checkbox'
+                    className='form-check-input ms-3'
+                    checked={task.done}
+                    onChange={() => {
+                        this.updateTaskDone(task)
+                    }} />
+                <button className='btn btn-primary ms-3'
+                    onClick={() => {
+                        this.setState({
+                            taskBeingEdited: task,
+                            modifiedTaskName: task.description
+                        })
+                    }}>Edit</button>
+            </li>
+        )
+    }
+
+    displayEditTask = () => {
         return(
+            <li className='mt-3'>
+                <input type='text' name='modifiedTaskName' value={this.state.modifiedTaskName} onChange={this.updateFormField}/>
+                <button className='btn btn-primary btn-sm ms-3'
+                        onClick={this.updateTask}>Update</button>
+            </li>
+        )
+    }
+
+    updateTask = () => {
+        let modifiedTask = {
+            ...this.state.taskBeingEdited, description: this.state.modifiedTaskName
+        }
+
+        let index = this.state.task.findIndex((task) => {
+            return task._id === modifiedTask._id
+        })
+
+        let cloned = this.state.task.slice()
+
+        cloned.splice(index, 1, modifiedTask);
+
+        this.setState({
+            task: cloned,
+            taskBeingEdited: null
+        })
+    }
+
+    render() {
+        return (
             <React.Fragment>
                 <h1>To Do List</h1>
                 {
                     this.state.task.map((obj) => {
                         return (
                             <React.Fragment key={obj._id}>
-                                <li>
-                                    {obj.description}
-                                    <input type='checkbox' 
-                                           className='form-check-input ms-3' 
-                                           checked={obj.done} 
-                                           onChange={() => {
-                                            this.updateTaskDone(obj)
-                                           }}/>
-                                </li>
+
+
+                                {this.state.taskBeingEdited === null || this.state.taskBeingEdited._id !== obj._id ?
+
+                                    this.displayTask(obj)
+                                    :
+                                    this.displayEditTask()
+
+                                }
                             </React.Fragment>
                         )
                     })
@@ -83,11 +141,11 @@ export default class TaskList extends React.Component{
                 <h2 className='mt-3'>Add a new task</h2>
                 <div>
                     <label>Task Name: </label>
-                    <input type='text' 
-                           className='form-control' 
-                           name='newTaskName' 
-                           value={this.state.newTaskName} 
-                           onChange={this.updateFormField}/>
+                    <input type='text'
+                        className='form-control'
+                        name='newTaskName'
+                        value={this.state.newTaskName}
+                        onChange={this.updateFormField} />
                     <button className='mt-2 btn btn-primary' onClick={this.addNewTask}>Add</button>
                 </div>
             </React.Fragment>
